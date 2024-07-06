b043149ca7d58d5c4c07c6b9a535554035369fe4
nihalmohdkp <kvnihal@gmail.com>
2024-07-06 10:42:46 +0530

    initial

---
+++ new.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_5tu4dod";
+    const templateID = "template_l8dez3k";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
